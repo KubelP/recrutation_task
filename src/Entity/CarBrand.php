a0d0581c5bbb2d460f108fe37e1148382c48be11
Quantum <jakubpiergies@gmail.com>
2022-12-02 15:13:49 +0100

    configuration of db in mysql

---
+++ src/Entity/CarBrand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarBrandRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarBrandRepository::class)]
+class CarBrand
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand_name = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrandName(): ?string
+    {
+        return $this->brand_name;
+    }
+
+    public function setBrandName(string $brand_name): self
+    {
+        $this->brand_name = $brand_name;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
