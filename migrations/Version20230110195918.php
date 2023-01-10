118af279aced6f3a9b37f5f5f825f775ceccf687
Quantum <jakubpiergies@gmail.com>
2023-01-10 21:00:13 +0100

    added updated mgration

---
+++ migrations/Version20230110195918.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230110195918 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car_brand CHANGE brandname brand_name VARCHAR(40) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car_brand CHANGE brand_name brandname VARCHAR(40) NOT NULL');
+    }
+}
